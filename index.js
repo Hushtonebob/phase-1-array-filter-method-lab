67a8eb52edea548c83e64870f79df12ea9cbce1b
BobbyDeluna <bobby.deluna2@gmail.com>
2022-06-08 13:48:15 -0700

    commit tree 816f440e43b730262717012ca26bdc5465c43d1c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah']
+
+
+function findMatching(arry,string){
+    let newDrivers = arry.filter(blam);
+    function blam(string){
+        return string = true; 
+    }
+        return newDrivers;
+};
